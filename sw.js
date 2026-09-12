a73c4ee6b93cb566203be72d59e56b069bc58b4b
phasita <phasita.s1997@gmail.com>
2026-09-12 23:12:09 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
-self.addEventListener("install", () => self.skipWaiting());
+{
+  "name": "QC Coconut RM",
+  "short_name": "QC RM",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#16a34a",
+  "icons": [
+    {
+      "src": "94D26D09-AAC6-4A52-BC35-24EDFFC6F659.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
